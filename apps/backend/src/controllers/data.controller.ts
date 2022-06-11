c80aff72b7e5e9b6f5a957b51df25c0f133c166e
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-06-11 21:35:47 +0200

    backend: added lightrule UNFINISHED

--- apps/backend/src/controllers/data.controller.ts
+++ apps/backend/src/controllers/data.controller.ts
@@ -1,7 +1,8 @@
 import { catchAsync } from '@infotition/express-error-handler';
 import httpStatus from 'http-status';
 import * as dataService from '@Services/data.service';
-
+import { Prisma, prisma as prismaclient} from '@prisma/client';
+import { prisma } from '@Config/index';
 
 export const data = catchAsync(async (req, res) => {
 
@@ -17,4 +18,22 @@ export const names = catchAsync(async (req, res) => {
 
     const data = await dataService.compileNames();
     res.status(httpStatus.OK).send(data);
+    });
+
+
+export const rules = catchAsync(async (req, res) => {
+
+    let hourStartInput = req.body.hourStart;
+    let minuteStartInput = req.body.minuteStart;
+    let secondStartInput = req.body.secondStart;
+
+    let hourEndInput = req.body.hourEnd;
+    let minuteEndInput = req.body.minuteEnd;
+    let secondEndInput = req.body.secondEnd;
+
+    prisma.lightRule.create({hourStart: hourStartInput, minuteStart: minuteStartInput, secondStart: secondStartInput, hourEnd: hourEndInput, minuteEnd: minuteEndInput, secondEnd: secondEndInput});
+
+    res.status(httpStatus.OK);
+
+
     });
